3a8d802abca8bd8372bd1ade25827d087fcf0613
filip-we <filip.wettlen@gmail.com>
2024-09-07 16:06:38 +0200

    Added install script and defaults

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "This script will setup e-ink-console on your Linux machine!"
+
+# Get submodules?
+
+cargo --version > /dev/null
+# Install cargo
+if [ $? -eq 127 ]; then
+    echo "You need Cargo to compile the drivers. Trying to install Cargo..."
+    curl https://sh.rustup.rs -sSf | sh
+else
+    echo "Cargo is installed!"
+fi
+
+echo "Compiling screen drivers..."
+cd rust-it8951
+cargo build --example cli
+cd ..
+
+echo "Creating Python virtual environment..."
+python -m venv .venv
+source .venv/bin/activate
+echo "Installing Ppython requirements..."
+pip install -r requirements.txt
+
+echo "Createing systemd-service-file..."
+CURRENT_DIR=`pwd`
+
+cat <<EOF > e-ink-console.service
+[Unit]
+Description=E-Ink Console
+
+[Service]
+Type=notify
+NotifyAccess=all
+ExecStart=${CURRENT_DIR}/.venv/bin/python -m e_ink_console
+Environment=PYTHONUNBUFFERED=true PYTHONPATH=${CURRENT_DIR}
+EOF
+
+
+echo "Installing systemd-service file - this needs root privileges!"
+sudo cp e-ink-console.service /usr/lib/systemd/system/e-ink-console.service
+sudo systemctl daemon-reload
+
+echo "e-ink-console successfully installed! Start the service with 'sudo systemctl start e-ink-console.service'!"
