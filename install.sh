0cb494d6e3c1327e7e03bd78daa4792f1b53ae2c
filip-we <filip.wettlen@gmail.com>
2024-09-28 15:01:46 +0000

    Improved install script. Added missing software, improved systemd-service to start before login-scree.

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ cargo --version > /dev/null
 if [ $? -eq 127 ]; then
     echo "You need Cargo to compile the drivers. Trying to install Cargo..."
     curl https://sh.rustup.rs -sSf | sh
+    echo "Installing base-devel to get compiler-stuff..."
+    sudo pacman -Syu base-devel
 else
     echo "Cargo is installed!"
 fi
@@ -35,13 +37,17 @@ Description=E-Ink Console
 [Service]
 Type=notify
 NotifyAccess=all
-ExecStart=${CURRENT_DIR}/.venv/bin/python -m e_ink_console
+ExecStart=${CURRENT_DIR}/.venv/bin/python -m e_ink_console --terminal-nr 1
 Environment=PYTHONUNBUFFERED=true PYTHONPATH=${CURRENT_DIR}
+
+[Install]
+WantedBy=getty.target
 EOF
 
 
 echo "Installing systemd-service file - this needs root privileges!"
 sudo cp e-ink-console.service /usr/lib/systemd/system/e-ink-console.service
 sudo systemctl daemon-reload
+sudo systemctl enable e-ink-console.service
 
 echo "e-ink-console successfully installed! Start the service with 'sudo systemctl start e-ink-console.service'!"
