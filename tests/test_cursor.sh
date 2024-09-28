e8731b75aca10f54766303294421e33bf91d9df2
filip-we <filip.wettlen@gmail.com>
2024-09-28 22:04:03 +0200

    Improved test. Now the test can reproduce some of the errors I see when using the screen as an actual console.

--- tests/test_cursor.sh
+++ tests/test_cursor.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-ESC="\033["
+ESC="\x1B["
 
-CLEAR="\033[2J"
-CURSOR_HOME="\033[H"
+CLEAR="\x1B[2J"
+CURSOR_HOME="\x1B[H"
+BACKSPACE="\x08"
+
 VAR="Cursor at Home position."
 
 TESTS=(
 #"${CURSOR_HOME}Hello, world!"
-"${CLEAR}\033[0;0H0        1         2         3         4         5         6         7         8\033[10;0H1\033[20;0H2\033[30;0H3\033[39;0H39"
+"${CLEAR}\x1B[0;0H0        1         2         3         4         5         6         7         8\x1B[10;0H1\x1B[20;0H2\x1B[30;0H3\x1B[39;0H39"
 "hej hej"
 "hej igen"
 "${CLEAR}${CURSOR_HOME}Let's clean the screen and write some stuff."
+"${CLEAR}${CURSOR_HOME}Clean the screen, then let's send four backspaces! 1234"
+"${BACKSPACE}${BACKSPACE}${BACKSPACE}${BACKSPACE}"
+"Now we should be printing neatly after the exclamation mark."
 )
 
 for string in "${TESTS[@]}"
 do
     read -p "Press ENTER to run next test."
-    echo -e "${string}" > $1
+    echo -en "${string}" > $1
 done
